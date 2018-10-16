231d4befe0e0958f3bdbac6816ef17ad4b4ed8eb
ChaoticWeg <shawn@chaoticweg.cc>
2018-10-16 12:41:57 -0500

    actually pipe integers out to file....

--- out/generate.sh
+++ out/generate.sh
@@ -6,11 +6,11 @@ pushd "${THIS_DIR}" >/dev/null 2>&1
 DATA_DIR="${THIS_DIR}/../data"
 
 # generate unique ints
-[[ ! -f "${DATA_DIR}/1k_unique.dat" ]] && ./generate.out --count 100000  --type unique
-[[ ! -f "${DATA_DIR}/5k_unique.dat" ]] && ./generate.out --count 500000  --type unique
-[[ ! -f "${DATA_DIR}/1m_unique.dat" ]] && ./generate.out --count 1000000 --type unique
+[[ ! -f "${DATA_DIR}/100k_unique.dat" ]] && ./generate.out --count 100000  --type unique > "${DATA_DIR}/100k_unique.dat"
+[[ ! -f "${DATA_DIR}/500k_unique.dat" ]] && ./generate.out --count 500000  --type unique > "${DATA_DIR}/500k_unique.dat"
+[[ ! -f "${DATA_DIR}/1m_unique.dat"   ]] && ./generate.out --count 1000000 --type unique > "${DATA_DIR}/1m_unique.dat"
 
 # generate limited-range ints
-[[ ! -f "${DATA_DIR}/1k_limited.dat" ]] && ./generate.out --count 100000   --type limited
-[[ ! -f "${DATA_DIR}/5k_limited.dat" ]] && ./generate.out --count 500000   --type limited
-[[ ! -f "${DATA_DIR}/1m_limited.dat" ]] && ./generate.out --count 1000000  --type limited
+[[ ! -f "${DATA_DIR}/100k_limited.dat" ]] && ./generate.out --count 100000   --type limited > "${DATA_DIR}/100k_limited.dat"
+[[ ! -f "${DATA_DIR}/500k_limited.dat" ]] && ./generate.out --count 500000   --type limited > "${DATA_DIR}/500k_limited.dat"
+[[ ! -f "${DATA_DIR}/1m_limited.dat"   ]] && ./generate.out --count 1000000  --type limited > "${DATA_DIR}/1m_limited.dat"
