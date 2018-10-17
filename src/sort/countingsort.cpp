a4e3031e73f41fcd3502feb185bc7f2ff05fbb41
ChaoticWeg <shawn@chaoticweg.cc>
2018-10-17 13:48:02 -0500

    working on sorting

--- src/sort/countingsort.cpp
+++ src/sort/countingsort.cpp
@@ -3,6 +3,9 @@
 #include <map>
 
 #include "ints_in.hpp"
+#include "ints_out.hpp"
+
+void CountingSort(const std::list<int> &ints, std::map<int, int> &buckets);
 
 int main(int argc, char **argv)
 {
@@ -12,12 +15,29 @@ int main(int argc, char **argv)
         return 1;
     }
 
+
+    // read ints from file
     std::list<int> ints;
     IntsIn::ReadFile(argv[1], ints);
 
     std::cerr << "count-sorting " << ints.size() << " ints\n";
 
+
+    // sort
     std::map<int, int> buckets;
+    CountingSort(ints, buckets);
+    
+
+    // print to STDOUT
+    IntsOut::PrintMap(buckets);
+
+    return 0;
+}
+
+void CountingSort(const std::list<int> &ints, std::map<int, int> &buckets)
+{
+    buckets.clear();
+
     for (auto it = ints.begin(); it != ints.end(); it++)
     {
         int thisInt = *it;
@@ -34,6 +54,4 @@ int main(int argc, char **argv)
         int existingCount = found->second;
         buckets[thisInt] = existingCount + 1;
     }
-
-    return 0;
 }
--- src/sort/quicksort.cpp
+++ src/sort/quicksort.cpp
@@ -2,6 +2,10 @@
 #include <list>
 
 #include "ints_in.hpp"
+#include "ints_out.hpp"
+
+void QuickSort(std::list<int> ints);      // quicksort
+void InsertionSort(std::list<int> ints);  // insertion sort
 
 int main(int argc, char **argv)
 {
@@ -11,10 +15,31 @@ int main(int argc, char **argv)
         return 1;
     }
 
+
+    // read ints from file
+
     std::list<int> ints;
     IntsIn::ReadFile(argv[1], ints);
 
+
+    // sort
+
     std::cerr << "quicksorting " << ints.size() << " ints\n";
+    QuickSort(ints);
+
+
+    // print to STDOUT
+    IntsOut::PrintList(ints);
 
     return 0;
+}
+
+void QuickSort(std::list<int> ints)
+{
+    // TODO QuickSort
+}
+
+void InsertionSort(std::list<int> ints)
+{
+    // TODO insertion sort
 }
