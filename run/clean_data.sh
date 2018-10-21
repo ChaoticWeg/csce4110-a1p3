b58eb2e4d11674c4db2565ede3065fad036fb039
ChaoticWeg <shawn@chaoticweg.cc>
2018-10-21 18:08:52 -0500

    remove all pushd/popd and clean_cd calls entirely
    stop fucking deleting my entire repo you idiot

--- run/clean_data.sh
+++ run/clean_data.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-DATA_DIR="$(readlink -f "${THIS_DIR}/../data")"
-
-# function: clean cd
-
-clean_cd() { for datafile in $(find . -maxdepth 1 -type f); do rm -f "$(readlink -f $datafile)"; done; };
+DATA_DIR="${THIS_DIR}/../data"
 
 # clean any stray data
-
-pushd "${DATA_DIR}" >/dev/null 2>&1
-clean_cd
+for datafile in $(find "${DATA_DIR}" -maxdepth 1 -type f); do rm -f "$datafile"; done;
 
 # clean any sorted data
 
-pushd "${DATA_DIR}/sorted" >/dev/null 2>&1
-for dir in $(find . -maxdepth 1 -type d); do
-    DIR_PATH="$(readlink -f $dir)"
-    pushd "${DIR_PATH}" >/dev/null 2>&1
-    clean_cd
-    popd >/dev/null 2>&1
+for datadir in $(find "${DATA_DIR}/sorted" -maxdepth 1 -type d); do
+    for datafile in $(find "${datadir}" -maxdepth 1 -type f); do rm -f "$datafile"; done;
 done
-popd >/dev/null 2>&1
-
-# clean up
-
-popd >/dev/null 2>&1
